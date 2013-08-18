096524fffd3fd76abb03834e76a119aebcb87fd5
Paulo <prrlara@gmail.com>
2013-08-18 05:58:37 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CapetaDemo::Application.config.secret_key_base = '80b30555aa0c5681f3ba5b83d6558900b62707ef7fd506ae3d0e08f422144ec58f32d73df7b7d2bae768ceed87801d90566dfd9aaf51763d5310db2cfc239d21'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CapetaDemo::Application.config.session_store :cookie_store, key: '_capeta_demo_session'
